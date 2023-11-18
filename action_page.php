59b1e2d6b54d208d4eb69c9955537ba09a96c260
Lorenzo Ortiz <LorenzoDavidOrtiz@gmail.com>
2023-11-18 14:41:55 -0800

    remove attatchments link

--- action_page.php
+++ action_page.php
@@ -28,8 +28,7 @@ try {
     $mail->addReplyTo('info@lorenzodortiz.com', 'Portfolio');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
