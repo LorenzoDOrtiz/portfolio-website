e7940b52a17f6099a65ee24abd242238cd89852a
Lorenzo Ortiz <LorenzoDavidOrtiz@gmail.com>
2023-11-18 14:58:01 -0800

    Adding more data to sent emails.

--- action_page.php
+++ action_page.php
@@ -1,42 +1,48 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+// Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
+// Load Composer's autoloader
 require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
+// Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.titan.email';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'contact@lorenzodortiz.com';                     //SMTP username
-    $mail->Password   = 'W2vZ0uiFUHPF&aa1';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    // Server settings
+    $mail->isSMTP();                // Send using SMTP
+    $mail->Host       = 'smtp.titan.email';  // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;        // Enable SMTP authentication
+    $mail->Username   = 'contact@lorenzodortiz.com';  // SMTP username
+    $mail->Password   = 'W2vZ0uiFUHPF&aa1';  // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable implicit TLS encryption
+    $mail->Port       = 465;         // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
+    // Recipients
     $mail->setFrom('contact@lorenzodortiz.com', 'Portfolio');
-    $mail->addAddress('contact@lorenzodortiz.com', 'Portfolio');     //Add a recipient
+    $mail->addAddress('contact@lorenzodortiz.com', 'Portfolio');  // Add a recipient
     $mail->addReplyTo('contact@lorenzodortiz.com', 'Portfolio');
 
-    //Attachments
-    
+    // Attachments
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    // Content
+    $mail->isHTML(true);  // Set email format to HTML
+
+    // Retrieve the form fields
+    $firstname = $_POST['firstname'];
+    $lastname  = $_POST['lastname'];
+    $subject   = $_POST['subject'];
+
+    // Set the email subject and body
+    $mail->Subject = $subject;
+    $mail->Body    = "First Name: $firstname<br>Last Name: $lastname<br>Subject: $subject";  // Include first and last name in the email body
+    $mail->AltBody = "First Name: $firstname\nLast Name: $lastname\nSubject: $subject";  // Plain text version
 
     $mail->send();
     echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+?>
