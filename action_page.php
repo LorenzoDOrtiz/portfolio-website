26b61e9eff36e38e8afa6b319d717d7003877805
Lorenzo Ortiz <LorenzoDavidOrtiz@gmail.com>
2023-11-18 12:30:35 -0800

    Adds email functionality for contact form and user confirmation messages.

--- action_page.php
+++ action_page.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$confirmation_message = ""; // Initialize the confirmation message variable
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Collect form data
     $fname = $_POST["firstname"];
@@ -9,14 +14,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Send an email (you may need to configure your server for this)
     $to = "contact@lorenzodortiz.com"; // Replace with your email address
-    $subject = "New Contact Form Submission";
+    $email_subject = "New Contact Form Submission";
     $message = "First Name: $fname\nLast Name: $lname\nSubject: $subject";
     $headers = "From: $fname $lname <$to>";
 
-    mail($to, $subject, $message, $headers);
+    // Send email and check for success
+    $success = mail($to, $email_subject, $message, $headers);
 
-    // Redirect to a thank you page or display a thank you message
-    header("Location: thank_you.html");
-    exit();
+    // Check if email was sent successfully
+    if ($success) {
+        $confirmation_message = "Thank you! Your message has been sent successfully.";
+    } else {
+        $confirmation_message = "Error sending email. Please try again.";
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <!-- Add your CSS styles or link to an external stylesheet here -->
+</head>
+<body>
+
+<?php echo $confirmation_message; ?>
+
+<!-- Your HTML form goes here -->
+<form method="post" action="your_php_script.php">
+    <!-- form fields go here -->
+    <label for="firstname">First Name:</label>
+    <input type="text" id="firstname" name="firstname" required>
+
+    <label for="lastname">Last Name:</label>
+    <input type="text" id="lastname" name="lastname" required>
+
+    <label for="subject">Subject:</label>
+    <textarea id="subject" name="subject" required></textarea>
+
+    <input type="submit" value="Submit">
+</form>
+
+<!-- Add any additional HTML content or scripts as needed -->
+
+</body>
+</html>
