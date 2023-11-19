bb102e503b3fbe31b13dc707d37abc4389d6c3dc
Lorenzo Ortiz <LorenzoDavidOrtiz@gmail.com>
2023-11-19 00:11:09 -0800

    test

--- send_email.php
+++ send_email.php
@@ -1,35 +1,60 @@
 <?php
+// Include the PHPMailer library
+require 'vendor/autoload.php';
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
+// Other email configuration
+$sender_email = 'contact@lorenzodortiz.com';
+$sender_password = '69Zhe!as*sDrAilA';
 
-require "vendor/autoload.php";
+// SMTP (sending) server details
+$smtp_server = 'smtp.titan.email';
+$smtp_port = 587;
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+// Retrieve form values from the submission
+$user_name = isset($_POST['user_name']) ? $_POST['user_name'] : '';
+$user_email = isset($_POST['user_email']) ? $_POST['user_email'] : '';
+$email_subject = isset($_POST['email_subject']) ? $_POST['email_subject'] : '';
+$email_body = isset($_POST['email_body']) ? $_POST['email_body'] : '';
 
-$mail = new PHPMailer(true);
+// Check if required fields are provided
+if (empty($user_name) || empty($user_email) || empty($email_subject) || empty($email_body)) {
+    die('Error: Please fill out all required fields.');
+}
 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+function send_email($user_name, $user_email, $email_subject, $email_body) {
+    global $sender_email, $sender_password, $smtp_server, $smtp_port;
 
-$mail->isSMTP();
-$mail->SMTPAuth = true;
+    // Create a PHPMailer object
+    $mail = new \PHPMailer\PHPMailer\PHPMailer();
 
-$mail->Host = "smtp.titan.email";
-$mail->SMTPSecure = PHPMailer::SSL;
-$mail->Port = 465;
+    try {
+        // Configure the SMTP settings
+        $mail->isSMTP();
+        $mail->Host = $smtp_server;
+        $mail->Port = $smtp_port;
+        $mail->SMTPAuth = true;
+        $mail->Username = $sender_email;
+        $mail->Password = $sender_password;
+        $mail->SMTPSecure = 'tls';
 
-$mail->Username = "contact@lorenzodortiz.com";
-$mail->Password = "69Zhe!as*sDrAilA";
+        // Set the email content
+        $mail->setFrom($user_email, $user_name); // Set the sender as the user's provided email and name
+        $mail->addAddress($sender_email); // Set the recipient as your email address
+        $mail->Subject = $email_subject;
+        $mail->Body = $email_body;
 
-$mail->setFrom($email, $name);
-$mail->addAddress("contact@lorenzodortiz.comm", "Portfolio");
+        // Send the email
+        if ($mail->send()) {
+            echo 'Email sent successfully.';
+        } else {
+            echo 'Error sending email: ' . $mail->ErrorInfo;
+            return;
+        }
+    } catch (Exception $e) {
+        echo 'Error sending email: ' . $e->getMessage();
+    }
+}
 
-$mail->Subject = $subject;
-$mail->Body = $message;
-
-$mail->send();
-
-header("Location: sent.html");
+// Call the function to send the email
+send_email($user_name, $user_email, $email_subject, $email_body);
+?>
